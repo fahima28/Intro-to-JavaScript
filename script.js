12e69d00bf523547859e0b9f640cae78da3936a2
Fahim A. <147541831+fahima28@users.noreply.github.com>
2024-03-12 18:02:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function myFunction() {
+  document.getElementById("greet").style.color = "blue";
+}
+function night(){
+  document.body.style.backgroundColor="darkBlue";
+  document.getElementById("headText").style.color = "lightyellow";
+  document.getElementById("headText1").style.color = "lightyellow";
+  document.getElementById("headText2").style.color = "lightyellow";
+  document.getElementById("headText3").style.color = "lightyellow";
+  document.getElementById("paragraph1").style.color = "lightyellow";
+}
+function light(){
+  document.body.style.backgroundColor="lightyellow";
+  document.getElementById("headText").style.color = "black";
+  document.getElementById("headText1").style.color = "black";
+  document.getElementById("headText2").style.color = "black";
+  document.getElementById("headText3").style.color = "black";
+  document.getElementById("paragraph1").style.color = "black";
+}
+function para(){
+document.getElementById("paragraph1").style.color = "purple"; document.getElementById("paragraph1").style.fontFamily = "cursive";
+}
